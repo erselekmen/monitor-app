1cb9b953995ab387447c635f68587886145eb301
ersel.ekmen <ersel.ekmen@xprclub.com>
2025-07-11 16:28:13 +0300

    test endpoint added

--- app/main.py
+++ app/main.py
@@ -3,3 +3,7 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 from fastapi.responses import Response
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hi, this is the Monitor App v1.0"}
